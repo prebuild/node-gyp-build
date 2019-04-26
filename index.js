61cd076a7932e4e8da971c47649763b6ccd797ff
Mathias Buus <mathiasbuus@gmail.com>
2019-04-26 16:39:53 +0200

    support PREBUILDS_ONLY env var

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var os = require('os')
 // Workaround to fix webpack's build warnings: 'the request of a dependency is an expression'
 var runtimeRequire = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require // eslint-disable-line
 
+var prebuildsOnly = !!process.env.PREBUILDS_ONLY
 var abi = process.versions.modules // TODO: support old node where this is undef
 var runtime = isElectron() ? 'electron' : 'node'
 var arch = os.arch()
@@ -27,11 +28,13 @@ load.path = function (dir) {
     if (process.env[name + '_PREBUILD']) dir = process.env[name + '_PREBUILD']
   } catch (err) {}
 
-  var release = getFirst(path.join(dir, 'build/Release'), matchBuild)
-  if (release) return release
+  if (!prebuildsOnly) {
+    var release = getFirst(path.join(dir, 'build/Release'), matchBuild)
+    if (release) return release
 
-  var debug = getFirst(path.join(dir, 'build/Debug'), matchBuild)
-  if (debug) return debug
+    var debug = getFirst(path.join(dir, 'build/Debug'), matchBuild)
+    if (debug) return debug
+  }
 
   // Find most specific flavor first
   var prebuilds = path.join(dir, 'prebuilds', platform + '-' + arch)
