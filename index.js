57be47b88bf4aea751bdbae93f90064eab5d960a
Kris Zyp <kriszyp@gmail.com>
2022-03-31 19:49:08 -0600

    Add support for finding prebuilds in platform-specific packages

--- index.js
+++ index.js
@@ -23,12 +23,11 @@ function load (dir) {
 
 load.path = function (dir) {
   dir = path.resolve(dir || '.')
-
+  var packageName = runtimeRequire(path.join(dir, 'package.json')).name
   try {
-    var name = runtimeRequire(path.join(dir, 'package.json')).name.toUpperCase().replace(/-/g, '_')
-    if (process.env[name + '_PREBUILD']) dir = process.env[name + '_PREBUILD']
+    var varName = packageName.toUpperCase().replace(/-/g, '_') + '_PREBUILD'
+    if (process.env[varName]) dir = process.env[varName]
   } catch (err) {}
-
   if (!prebuildsOnly) {
     var release = getFirst(path.join(dir, 'build/Release'), matchBuild)
     if (release) return release
@@ -43,6 +42,15 @@ load.path = function (dir) {
   var nearby = resolve(path.dirname(process.execPath))
   if (nearby) return nearby
 
+  var platformPackage = packageName + '-' + platform + '-' + arch
+  try {
+    var prebuildPackage = path.dirname(require('module').createRequire(path.join(dir, 'package.json')).resolve(platformPackage))
+    return resolveFile(prebuildPackage)
+  } catch(error) {
+    console.error(error)
+  }
+
+
   var target = [
     'platform=' + platform,
     'arch=' + arch,
@@ -56,16 +64,17 @@ load.path = function (dir) {
     typeof __webpack_require__ === 'function' ? 'webpack=true' : '' // eslint-disable-line
   ].filter(Boolean).join(' ')
 
-  throw new Error('No native build was found for ' + target + '\n    loaded from: ' + dir + '\n')
+  throw new Error('No native build was found for ' + target + '\n    loaded from: ' + dir + ' and package: ' + platformPackage + '\n')
 
   function resolve (dir) {
     // Find matching "prebuilds/<platform>-<arch>" directory
     var tuples = readdirSync(path.join(dir, 'prebuilds')).map(parseTuple)
     var tuple = tuples.filter(matchTuple(platform, arch)).sort(compareTuples)[0]
     if (!tuple) return
-
+    return resolveFile(path.join(dir, 'prebuilds', tuple.name))
+  }
+  function resolveFile (prebuilds) {
     // Find most specific flavor first
-    var prebuilds = path.join(dir, 'prebuilds', tuple.name)
     var parsed = readdirSync(prebuilds).map(parseTags)
     var candidates = parsed.filter(matchTags(runtime, abi))
     var winner = candidates.sort(compareTags(runtime))[0]
