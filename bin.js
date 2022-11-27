9bcfec92f9f825c3decf4e038f88e75567a43828
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-11-26 23:23:00 -0800

    fix: fix spawning node-gyp-build-test

--- bin.js
+++ bin.js
@@ -5,7 +5,7 @@ var os = require('os')
 var path = require('path')
 
 if (!buildFromSource()) {
-  proc.exec('node-gyp-build-test', function (err, stdout, stderr) {
+  proc.exec(process.execPath, ['node-gyp-build-test'], function (err, stdout, stderr) {
     if (err) {
       if (verbose()) console.error(stderr)
       preinstall()
