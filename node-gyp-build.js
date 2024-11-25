bb78a290daaa0f7e2ecd02ecfe092dd2b0e6dd00
Isaac Aderogba <isaacaderogba1@gmail.com>
2024-11-25 13:05:14 +0000

    fix: remove previously-defined abi variable

--- node-gyp-build.js
+++ node-gyp-build.js
@@ -8,7 +8,6 @@ var runtimeRequire = typeof __webpack_require__ === 'function' ? __non_webpack_r
 
 var vars = (process.config && process.config.variables) || {}
 var prebuildsOnly = !!process.env.PREBUILDS_ONLY
-var abi = process.versions.modules // TODO: support old node where this is undef
 var runtime = isElectron() ? 'electron' : (isNwjs() ? 'node-webkit' : 'node')
 var abi = process.env.npm_config_target ? nodeAbi.getAbi(process.env.npm_config_target, runtime) : process.versions.modules // TODO: support old node where this is undef
 
